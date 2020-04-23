0c81314a176d72435566c450de48027e4cd5af0b
Vadim Galaktionov <vadim.galaktionov@gmail.com>
2020-04-24 00:20:47 +0200

    Add stackoverflow fix

--- systray_browser_darwin.m
+++ systray_browser_darwin.m
@@ -2,6 +2,42 @@
 #import <WebKit/WebKit.h>
 #include "systray.h"
 
+@interface SystrayApplication: NSApplication
+  - (void) sendEvent:(NSEvent *)event {
+    if ([event type] == NSKeyDown) {
+        if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"x"]) {
+                if ([self sendAction:@selector(cut:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"]) {
+                if ([self sendAction:@selector(copy:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"]) {
+                if ([self sendAction:@selector(paste:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"z"]) {
+                if ([self sendAction:@selector(undo:) to:nil from:self])
+                    return;
+            }
+            else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"]) {
+                if ([self sendAction:@selector(selectAll:) to:nil from:self])
+                    return;
+            }
+        }
+        else if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == (NSCommandKeyMask | NSShiftKeyMask)) {
+            if ([[event charactersIgnoringModifiers] isEqualToString:@"Z"]) {
+                if ([self sendAction:@selector(redo:) to:nil from:self])
+                    return;
+            }
+        }
+    }
+    [super sendEvent:event];
+}
+@end
+
 NSWindowController *windowController = nil;
 NSWindow *window = nil;
 WKWebView *webView = nil;
@@ -13,7 +49,7 @@ void configureAppWindow(char* title, int width, int height)
     return;
   }
 
-  NSApplication *app = [NSApplication sharedApplication];
+  SystrayApplication *app = [SystrayApplication sharedApplication];
   [app setActivationPolicy:NSApplicationActivationPolicyRegular];
   [app activateIgnoringOtherApps:YES];
 
